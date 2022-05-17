489596a90af8a817677fa4c7836e7d8217b28530
carlafaes <carlafaes18@hotmail.com>
2022-05-17 02:41:22 -0300

    var echo

--- index.php
+++ index.php
@@ -3,10 +3,15 @@
 
   //variables
   $nombre_persona ="car";
-  $edad= 25 ;
+  $edad= 28;
   
   // ambos print logran el mismo resultado
   print "El nombre de usuario es " . $nombre_persona;
   print "El nombre de usuario es $nombre_persona";
+
+  //las comillas simples toman el string como literal, y no toma la variable
+  print 'El nombre de usuario es $nombre_persona';
   
+  //echo tambien nos permite imprimir
+  echo "El nombre de usuario es " . $nombre_persona . " su edad es " . $edad;
 ?>
