3a98ad3ffd15988069bb26489e80458a4923d6bc
carlafaes <carlafaes18@hotmail.com>
2022-05-17 15:20:48 -0300

    valores por valor y referencia

--- strings.php
+++ strings.php
@@ -16,4 +16,35 @@
      echo "los strings no coinciden";
  }
 
+
+ //pasar parametro por valor
+ function mayusChange($param){
+     $param= strtolower($param);
+
+     $param=ucwords($param);
+
+     return $param;
+ }
+
+ $cadena= "hEllO woRld";
+
+ echo mayusChange($cadena) . "<br>";
+ echo "la cadena es $cadena <br>";
+
+
+ //pasar valor por referencia
+ //con el signo & creamos un vinculo entre las variables $param y $cadena2,por lo que la variable $cadena2 tambien se ve afectada por este cambio
+ function mayusChangeDos(&$param){
+    $param= strtolower($param);
+
+    $param=ucwords($param);
+
+    return $param;
+}
+
+$cadena2= "hEllO woRld";
+
+echo mayusChangeDos($cadena2) . "<br>";
+echo "la cadena es $cadena2";
+
 ?>
