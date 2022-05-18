aef38cfab2b5408197102215fc19667cb5d671bf
carlafaes <carlafaes18@hotmail.com>
2022-05-18 17:19:57 -0300

    arrays

---
+++ arrays.php
@@ -0,0 +1,12 @@
+<?php
+//arrays asociativos
+
+$semana[]="Lunes";
+$semana[]="Martes";
+$semana[]="Miercoles";
+echo $semana[1];//imprime en pantalla "martes"
+
+//arrays indexados
+$semana2=array(1,2,3,4,5,6);
+echo $semana2[1];//imprime 2
+?>
--- index.php
+++ index.php
@@ -8,6 +8,8 @@
   // include("objetos.php");
   echo "<br>";
   include("objetos_dos.php");
+  echo "<br>";
+  include("arrays.php");
 
   // $compra_cliente= new Compra_vehiculo("compacto");
   // echo "el precio final es: " . $compra_cliente->precio_final;
