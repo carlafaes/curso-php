36ada2162e92bdecfb2f8312918e1fb0d2899bbb
carlafaes <carlafaes18@hotmail.com>
2022-05-18 17:25:21 -0300

    arrays

--- arrays.php
+++ arrays.php
@@ -6,6 +6,13 @@ $semana[]="Martes";
 $semana[]="Miercoles";
 echo $semana[1];//imprime en pantalla "martes"
 
+$datos=array(
+    "Nombre"=>"Juan",
+    "Apellido"=>"Perez",
+    "Edad"=>"24"
+);
+echo $datos["Apellido"];//imprime Perez
+
 //arrays indexados
 $semana2=array(1,2,3,4,5,6);
 echo $semana2[1];//imprime 2
